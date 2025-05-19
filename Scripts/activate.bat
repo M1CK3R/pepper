aa0479b921d0ecbe7d481e7cc199904c15301a5a
Erick Carbonell <112967607+M1CK3R@users.noreply.github.com>
2025-05-19 12:11:39 -0600

    Update activate.bat

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\PC\Desktop\Tareas\2024\Galileo\Seguridad Informatica\A8\.venv
+set VIRTUAL_ENV=environment_route\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
